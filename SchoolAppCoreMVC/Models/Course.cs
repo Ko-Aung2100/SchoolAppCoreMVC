4937606f36375ee632943f75b27c797e0d9f8521
aung kaung htet <aungkaunghtetps2100@gmail.com>
2025-04-28 16:45:38 +0700

    Add project files.

---
+++ SchoolAppCoreMVC/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace SchoolAppCoreMVC.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public required string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentID { get; set; }
+
+        public required Department Department { get; set; }
+        public  ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
---
+++ SchoolAppCoreMVC/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SchoolAppCoreMVC.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public decimal Grade { get; set; }
+
+        public required Course Course { get; set; }
+        public required Student Student { get; set; } 
+
+    }
+}
